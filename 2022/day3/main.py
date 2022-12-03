1269b73ada3ff704f9b8edb6d9f1ceda4b537d3e
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-12-02 22:39:19 -0800

    added day3

---
+++ 2022/day3/main.py
@@ -0,0 +1,41 @@
+INPUT = 'input'
+priorities = "0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def extract_input(filename):
+  file = open(filename, 'r')
+  return list(file.read().splitlines())
+
+def priority(lines):
+  score = 0   
+  for line in lines:
+    length = len(line)//2
+    first, second = set(line[:length]), set(line[length:])
+    letters = first.intersection(second)
+    score += sum([priorities.index(i) for i in letters])
+  return score    
+
+def priority2(lines):
+  score = 0
+  i = 0
+  for line in lines:
+    if i == 0:
+      first = set(line)
+    if i == 1:
+      second = set(line)
+    if i == 2:
+      third = set(line)
+      letters = first & second & third
+      score += sum([priorities.index(i) for i in letters])
+      i = 0
+      continue
+    i += 1
+  return score
+
+def main():
+  lines = extract_input(INPUT)
+  print(priority(lines))
+  print(priority2(lines))
+
+if __name__ == '__main__':
+  main()
